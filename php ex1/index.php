6f0e4ac6956c180a242cd70723d22efa1e9d68ad
Amalia <amalialorenaferariu@gmail.com>
2021-12-17 15:32:57 +0100

    code

---
+++ php ex1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <?php
+      $string = "Is this the real life? Is this just fantasy? Caught in a landslide No escape from reality";
+      $stringLength = strlen($string);
+  ?>
+  <p><?php echo $string ?></p> 
+  <p> String length is: <?php echo $stringLength ?></p> 
+  
+</body>
+</html>
